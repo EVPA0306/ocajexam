d74cc8cf77341d755df1ddf376fdc37f342ff83d
EVPA0306 <pavlenko.evgeny@gmail.com>
2016-02-29 16:50:14 -0700

    ExerciseOne java class added

---
+++ com/evpa/ocajexam/exercises/exerciseone/ExerciseOne.java
@@ -0,0 +1,31 @@
+package com.evpa.ocajexam.exercises.exerciseone;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
+
+public class ExerciseOne {
+	public static void main(String[] args) throws IOException {
+
+		Files.createDirectories(Paths.get("logs"));
+		DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMMdd_hhmm");
+		LocalDateTime now = LocalDateTime.now();
+		String date = now.format(df);
+		String logFileName = "logs/testlog-" + date + ".txt";
+		FileHandler myFileHandler = new FileHandler(logFileName);
+		myFileHandler.setFormatter(new SimpleFormatter());
+		Logger ocaJLogger = Logger.getLogger("OCAJ Logger");
+		ocaJLogger.setLevel(Level.ALL);
+		ocaJLogger.addHandler(myFileHandler);
+		ocaJLogger.info("\nThis is a logged information message");
+		myFileHandler.close();
+	}
+}
